3ef79485bbaf3588a46b65220c4dfa8e700e7343
adi <adindo48@gmail.com>
2023-07-15 20:23:25 +0700

    day10 golang

--- golang/main.go
+++ golang/main.go
@@ -1,1 +1,16 @@
-for()
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "adi"
+
+	lastName := "widiawan"
+
+	// first, second, third := "satu", "dua", "tiga",
+
+	// 	fmt.Println(first, second, third)
+	fmt.Printf("Hallo %s %s! \n", firstName, lastName)
+	// Menampilkan pesan
+	fmt.Println("Hello", "World", "Haha", "Gotcha")
+}
