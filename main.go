b521449e24faba5148a6457e60285097f4d10b3a
adi <adindo48@gmail.com>
2023-07-23 13:32:56 +0700

    day 12 editProject

--- main.go
+++ main.go
@@ -4,23 +4,32 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Project struct {
-	ProjectName string
-	Description string
-	Author      string
-	PostDate    string
+	Author       string
+	ProjectName  string
+	Durations    string
+	StartDate    string
+	EndDate      string
+	Description  string
+	Technologies []string
+	Image        string
 }
 
 var dataProject = []Project{
 	{
-		ProjectName: "Project 1",
-		Description: "ini Project 1",
-		Author:      "Adiwidiawan",
-		PostDate:    "20July2023",
+		Author:       "Adiwidiawan",
+		ProjectName:  "Project 1",
+		Durations:    Duration("2020-01-15", "2020-02-15"),
+		StartDate:    "2020-01-15",
+		EndDate:      "2020-02-15",
+		Description:  "HALOO GUYSSSSS",
+		Technologies: []string{"Golang"},
+		Image:        "adi.jpg",
 	},
 }
 
@@ -36,13 +45,12 @@ func main() {
 	e.GET("/projectDetail/:id", projectDetail)
 	e.POST("/addmyProject", addmyProject)
 	e.POST("/deletemyProject/:id", deletemyProject)
-	e.GET("/updateProject/:id", updateProject)
-	e.POST("/updatedProject", updatedProject)
+	e.GET("/updateProject/:id", editProject)
+	e.POST("/updatedProject/:id", updatedProject)
 
 	e.Logger.Fatal(e.Start("localhost:666"))
 }
 
-// handler
 func home(x echo.Context) error {
 	tmplate, err := template.ParseFiles("htmls/index.html")
 
@@ -97,10 +105,14 @@ func projectDetail(c echo.Context) error {
 	for index, data := range dataProject {
 		if index == idInt {
 			projectDetail = Project{
-				ProjectName: data.ProjectName,
-				Description: data.Description,
-				Author:      data.Author,
-				PostDate:    data.PostDate,
+				Author:       data.Author,
+				ProjectName:  data.ProjectName,
+				Durations:    data.Durations,
+				StartDate:    data.StartDate,
+				EndDate:      data.EndDate,
+				Description:  data.Description,
+				Technologies: []string{},
+				// Image:        "adi.jpg",
 			}
 		}
 	}
@@ -116,19 +128,24 @@ func projectDetail(c echo.Context) error {
 func addmyProject(c echo.Context) error {
 	projectName := c.FormValue("projectName")
 	description := c.FormValue("description")
-	// StartDate := c.FormValue("startdate")
-	// EndDate := c.FormValue("enddate")
+	StartDate := c.FormValue("startdate")
+	EndDate := c.FormValue("enddate")
 	// checklis1 := c.FormValue("checklis1")
 	// checklis2 := c.FormValue("checklis2")
 	// checklis3 := c.FormValue("checklis3")
 	// checklis4 := c.FormValue("checklis4")
 
 	newProject := Project{
-		ProjectName: projectName,
-		Description: description,
-		Author:      "Adiwidiawan",
-		PostDate:    "27 July 2023",
+		Author:       "Adiwidiawan",
+		ProjectName:  projectName,
+		Durations:    Duration(StartDate, EndDate),
+		StartDate:    StartDate,
+		EndDate:      EndDate,
+		Description:  description,
+		Technologies: []string{},
+		Image:        "",
 	}
+
 	dataProject = append(dataProject, newProject)
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
@@ -151,45 +168,90 @@ func deletemyProject(c echo.Context) error {
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
 
-func updateProject(x echo.Context) error {
-	tmplate, _ := template.ParseFiles("htmls/updateProject.html")
+func editProject(x echo.Context) error {
 	id := x.Param("id")
 	Id, _ := strconv.Atoi(id)
-	projectName := x.FormValue("projectName")
-	description := x.FormValue("description")
 
-	UpdatedProject := Project{
-		ProjectName: projectName,
-		Description: description,
-		Author:      "Adiwidiawan",
-		PostDate:    "27 July 2023",
-	}
+	ProjectEdit := Project{}
 
 	for index, data := range dataProject {
 		if index == Id {
-			UpdatedProject = Project{
-				ProjectName: data.ProjectName,
-				Description: data.Description,
+			ProjectEdit = Project{
 				Author:      data.Author,
-				PostDate:    data.PostDate,
+				ProjectName: data.ProjectName,
+				// Durations:    Duration(data.StartDate, data.EndDate),
+				StartDate:   data.StartDate,
+				EndDate:     data.EndDate,
+				Description: data.Description,
+				// Technologies: []string{},
+				// Image:        "",
 			}
 		}
 	}
+	data := map[string]interface{}{
+		"Project": ProjectEdit,
+		"Id":      id,
+	}
 
-	dataProject = append(dataProject[:Id], UpdatedProject)
+	tmpl, err := template.ParseFiles("htmls/updateProject.html")
 
-	return tmplate.Execute(x.Response(), nil)
+	if err != nil {
+		return x.JSON(500, err.Error())
+	}
+
+	return tmpl.Execute(x.Response(), data)
 }
+
 func updatedProject(x echo.Context) error {
+
+	id := x.Param("id")
+	Id, _ := strconv.Atoi(id)
+
+	projectName := x.FormValue("projectName")
+	description := x.FormValue("description")
+	StartDate := x.FormValue("startdate")
+	EndDate := x.FormValue("enddate")
+	// checklis1 := x.FormValue("checklis1")
+	// checklis2 := x.FormValue("checklis2")
+	// checklis3 := x.FormValue("checklis3")
+	// checklis4 := x.FormValue("checklis4")
+	// StartDatee, _ := time.Parse("2006-01-02", StartDate)
+	// EndDatee, _ := time.Parse("2006-01-02", EndDate)
+
+	UpdatedProject := Project{
+		Author:      "Adiwidiawan",
+		ProjectName: projectName,
+		Durations:   Duration(StartDate, EndDate),
+		StartDate:   StartDate,
+		EndDate:     EndDate,
+		Description: description,
+		// Technologies: []string{},
+		// Image:        Image,
+	}
+
+	dataProject[Id] = UpdatedProject
 
 	return x.Redirect(http.StatusMovedPermanently, "/")
 }
 
-// StartDate := c.FormValue("startdate")
-// EndDate := c.FormValue("enddate")
-// checklis1 := c.FormValue("checklis1")
-// checklis2 := c.FormValue("checklis2")
-// checklis3 := c.FormValue("checklis3")
-// checklis4 := c.FormValue("checklis4")
+func Duration(StartDate string, EndDate string) string {
+	StartDatee, _ := time.Parse("2006-01-02", StartDate)
+	EndDatee, _ := time.Parse("2006-01-02", EndDate)
 
-// return c.Redirect(http.StatusMovedPermanently, "/")
+	diff := EndDatee.Sub(StartDatee)
+	day := int(diff.Hours() / 24)
+	week := day / 7
+	month := day / 30
+	year := month / 12
+	if day < 7 {
+		return strconv.Itoa(day) + "day"
+	}
+	if week < 4 {
+		return strconv.Itoa(week) + "week"
+	}
+	if month < 4 {
+		return strconv.Itoa(month) + "month"
+	}
+	return strconv.Itoa(year) + "year"
+
+}
